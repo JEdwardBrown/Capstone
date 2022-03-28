6475fcdf6f9779ea79d237c734c589242a48cbfc
JBrown <jeffedwardbrown@gmail.com>
2022-03-27 20:48:01 -0400

    Continued Employee Backend

--- server/src/main/java/com/Application.java
+++ server/src/main/java/com/Application.java
@@ -1,60 +1,6 @@
 package com;
-import java.util.Scanner;
-import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
-        char choice;
-        Scanner kbd = new Scanner(System.in);
-        System.out.println("This is a test!");
-        System.out.println("I really hope this does what I want it to. Let's begin!");
-        do {
-            System.out.println("Please choose from one of the following options. Use the first letter of each " +
-                    "option to make your choice.");
-            System.out.println("Please pick from [V]iew Inventory, View [W]arehouse, [A]lter price," +
-                    " Add [I]nventory, or [Q]uit");
-            choice = kbd.nextLine().toUpperCase().charAt(0);
-            switch (choice) {
-                case 'V': ViewInventory();
-                    break;
-                case 'W': ViewWarehouse();
-                    break;
-                case 'A': AlterPrice();
-                    break;
-                case 'I': AddInventory();
-                    break;
-                case 'Q':
-                    break;
-                default:
-                    break;
-            }
-        }while(choice != 'Q');
-        System.out.println("Thank you, have a good day!");
-    }
 
-    public static void ViewInventory() {
-        int storeID;
-        Scanner kbd = new Scanner(System.in);
-        do {
-            System.out.println("Please enter the 3-digit id of the store whose inventory you are trying to access: ");
-            storeID = kbd.nextInt();
-            if(storeID < 100 || storeID > 999)
-                System.out.println("Invalid Store Number, please try again.");
-        }while(storeID < 100 || storeID > 999);
-
-    }
-
-    public static void ViewWarehouse() {
-
-    }
-
-    public static void AlterPrice() {
-
-    }
-
-    public static void AddInventory() {
-
-    }
 }
--- server/src/main/java/com/services/employee/EmployeeController.java
+++ server/src/main/java/com/services/employee/EmployeeController.java
@@ -2,15 +2,32 @@ package com.services.employee;
 
 
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.netty.channel.EventLoopGroup;
+
+import java.util.concurrent.ExecutorService;
 
 @Controller("/services/employee")
 public class EmployeeController {
     private final EmployeeServices employeeServices;
+    private final EventLoopGroup eventLoopGroup;
+    private final ExecutorService executorService;
 
-    public FeedbackTemplateController(employeeServices feedbackTemplateServices,
-                                      EventLoopGroup eventLoopGroup, ExecutorService executorService) {
-        this.employeeServices = feedbackTemplateServices;
+    public EmployeeController(EmployeeServices employeeServices,
+                              EventLoopGroup eventLoopGroup, ExecutorService executorService) {
+        this.employeeServices = employeeServices;
         this.eventLoopGroup = eventLoopGroup;
         this.executorService = executorService;
     }
+
+    @Post()
+    public Single<HttpResponse<FeedbackTemplateResponseDTO>> save(@Body @Valid @NotNull FeedbackTemplateCreateDTO requestBody) {
+        return Single.fromCallable(() -> feedbackTemplateServices.save(fromDTO(requestBody)))
+                .observeOn(Schedulers.from(eventLoopGroup))
+                .map(savedTemplate -> (HttpResponse<FeedbackTemplateResponseDTO>) HttpResponse
+                        .created(fromEntity(savedTemplate))
+                        .headers(headers -> headers.location(URI.create("/feedback_templates/" + savedTemplate.getId()))))
+                .subscribeOn(Schedulers.from(executorService));
+    }
+
 }
